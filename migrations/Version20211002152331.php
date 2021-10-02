00bab3f491ee73e328272bd900f43b6bb3adcde1
IsaacLaifer <laiferisaac@gmail.com>
2021-10-02 17:44:05 +0200

    database
    création et lien a la bdd

---
+++ migrations/Version20211002152331.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211002152331 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE document (id INT AUTO_INCREMENT NOT NULL, document VARCHAR(255) NOT NULL, timestamp DATETIME NOT NULL, id_type INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE type (id INT AUTO_INCREMENT NOT NULL, document_id INT NOT NULL, type VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, INDEX IDX_8CDE5729C33F7837 (document_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE type ADD CONSTRAINT FK_8CDE5729C33F7837 FOREIGN KEY (document_id) REFERENCES document (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE type DROP FOREIGN KEY FK_8CDE5729C33F7837');
+        $this->addSql('DROP TABLE document');
+        $this->addSql('DROP TABLE type');
+    }
+}
